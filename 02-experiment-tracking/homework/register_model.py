941976e5cb6d097ca60af3cf3bb60032f43b7006
K M Jawadur Rahman <kmjawadurrahman@Ks-MacBook-Air.local>
2022-06-03 02:21:21 +0600

    experiment tracking homework

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -18,11 +18,11 @@ mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
 SPACE = {
-    'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
-    'n_estimators': scope.int(hp.quniform('n_estimators', 10, 50, 1)),
-    'min_samples_split': scope.int(hp.quniform('min_samples_split', 2, 10, 1)),
-    'min_samples_leaf': scope.int(hp.quniform('min_samples_leaf', 1, 4, 1)),
-    'random_state': 42
+    "max_depth": scope.int(hp.quniform("max_depth", 1, 20, 1)),
+    "n_estimators": scope.int(hp.quniform("n_estimators", 10, 50, 1)),
+    "min_samples_split": scope.int(hp.quniform("min_samples_split", 2, 10, 1)),
+    "min_samples_leaf": scope.int(hp.quniform("min_samples_leaf", 1, 4, 1)),
+    "random_state": 42,
 }
 
 
@@ -58,32 +58,39 @@ def run(data_path, log_top):
         experiment_ids=experiment.experiment_id,
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=log_top,
-        order_by=["metrics.rmse ASC"]
+        order_by=["metrics.rmse ASC"],
     )
     for run in runs:
         train_and_log_model(data_path=data_path, params=run.data.params)
 
     # select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse ASC"],
+    )[0]
 
     # register the best model
-    # mlflow.register_model( ... )
+    run_id = best_run.info.run_id
+    model_uri = f"runs:/{run_id}/model"
+    mlflow.register_model(model_uri=model_uri, name="nyc-taxi-random-forest")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--data_path",
         default="./output",
-        help="the location where the processed NYC taxi trip data was saved."
+        help="the location where the processed NYC taxi trip data was saved.",
     )
     parser.add_argument(
         "--top_n",
         default=5,
         type=int,
-        help="the top 'top_n' models will be evaluated to decide which model to promote."
+        help="the top 'top_n' models will be evaluated to decide which model to promote.",
     )
     args = parser.parse_args()
 
--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -4,6 +4,11 @@ import pickle
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import mlflow
+
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
 
 
 def load_pickle(filename: str):
@@ -17,20 +22,23 @@ def run(data_path):
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
     rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+    with mlflow.start_run():
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--data_path",
         default="./output",
-        help="the location where the processed NYC taxi trip data was saved."
+        help="the location where the processed NYC taxi trip data was saved.",
     )
     args = parser.parse_args()
+
+    mlflow.sklearn.autolog()
 
     run(args.data_path)
