718a769a1f162ff15095257e745607a6544a2a12
K M Jawadur Rahman <kmjawadurrahman@Ks-MacBook-Air.local>
2022-06-12 01:57:10 +0600

    module 3 homework

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -1,16 +1,24 @@
-import pandas as pd
+import pickle
+import datetime
+from dateutil.relativedelta import relativedelta
 
+import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from prefect import flow, task, get_run_logger
 
+
+@task
 def read_data(path):
     df = pd.read_parquet(path)
     return df
 
+
+@task
 def prepare_features(df, categorical, train=True):
-    df['duration'] = df.dropOff_datetime - df.pickup_datetime
-    df['duration'] = df.duration.dt.total_seconds() / 60
+    df["duration"] = df.dropOff_datetime - df.pickup_datetime
+    df["duration"] = df.duration.dt.total_seconds() / 60
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     mean_duration = df.duration.mean()
@@ -18,41 +26,70 @@ def prepare_features(df, categorical, train=True):
         print(f"The mean duration of training is {mean_duration}")
     else:
         print(f"The mean duration of validation is {mean_duration}")
-    
-    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+
+    df[categorical] = df[categorical].fillna(-1).astype("int").astype("str")
     return df
 
+
+@task
 def train_model(df, categorical):
 
-    train_dicts = df[categorical].to_dict(orient='records')
+    logger = get_run_logger()
+
+    train_dicts = df[categorical].to_dict(orient="records")
     dv = DictVectorizer()
-    X_train = dv.fit_transform(train_dicts) 
+    X_train = dv.fit_transform(train_dicts)
     y_train = df.duration.values
 
-    print(f"The shape of X_train is {X_train.shape}")
-    print(f"The DictVectorizer has {len(dv.feature_names_)} features")
+    logger.info(f"The shape of X_train is {X_train.shape}")
+    logger.info(f"The DictVectorizer has {len(dv.feature_names_)} features")
 
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_train)
     mse = mean_squared_error(y_train, y_pred, squared=False)
-    print(f"The MSE of training is: {mse}")
+    logger.info(f"The MSE of training is: {mse}")
     return lr, dv
 
+
+@task
 def run_model(df, categorical, dv, lr):
-    val_dicts = df[categorical].to_dict(orient='records')
-    X_val = dv.transform(val_dicts) 
+
+    logger = get_run_logger()
+
+    val_dicts = df[categorical].to_dict(orient="records")
+    X_val = dv.transform(val_dicts)
     y_pred = lr.predict(X_val)
     y_val = df.duration.values
 
     mse = mean_squared_error(y_val, y_pred, squared=False)
-    print(f"The MSE of validation is: {mse}")
+    logger.info(f"The MSE of validation is: {mse}")
     return
 
-def main(train_path: str = './data/fhv_tripdata_2021-01.parquet', 
-           val_path: str = './data/fhv_tripdata_2021-02.parquet'):
 
-    categorical = ['PUlocationID', 'DOlocationID']
+@task
+def get_paths(date):
+    if date is None:
+        date = datetime.date.today()
+    else:
+        date = datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    train_date = date - relativedelta(months=+2)
+    val_date = date - relativedelta(months=+1)
+
+    year_month_str_train = train_date.strftime("%Y-%m-%d")[:-3]
+    year_month_str_val = val_date.strftime("%Y-%m-%d")[:-3]
+    train_path = f"./data/fhv_tripdata_{year_month_str_train}.parquet"
+    val_path = f"./data/fhv_tripdata_{year_month_str_val}.parquet"
+
+    return train_path, val_path
+
+
+@flow
+def main(date=None):
+    train_path, val_path = get_paths(date).result()
+
+    categorical = ["PUlocationID", "DOlocationID"]
 
     df_train = read_data(train_path)
     df_train_processed = prepare_features(df_train, categorical)
@@ -61,7 +98,30 @@ def main(train_path: str = './data/fhv_tripdata_2021-01.parquet',
     df_val_processed = prepare_features(df_val, categorical, False)
 
     # train the model
-    lr, dv = train_model(df_train_processed, categorical)
+    lr, dv = train_model(df_train_processed, categorical).result()
+
+    if date is None:
+        date = datetime.date.today().strftime("%Y-%m-%d")
+
+    with open(f"preprocessor/dv-{date}.b", "wb") as f_out:
+        pickle.dump(dv, f_out)
+
+    with open(f"models/model-{date}.bin", "wb") as f_out:
+        pickle.dump(lr, f_out)
+
     run_model(df_val_processed, categorical, dv, lr)
 
-main()
+
+# main(date="2021-08-15")
+
+from prefect.deployments import DeploymentSpec
+from prefect.orion.schemas.schedules import CronSchedule
+from prefect.flow_runners import SubprocessFlowRunner
+
+DeploymentSpec(
+    name="cron-model-training",
+    flow=main,
+    schedule=CronSchedule(cron="0 9 15 * *", timezone="America/New_York"),
+    flow_runner=SubprocessFlowRunner(),
+    parameters={"date": "2021-08-15"},
+)
